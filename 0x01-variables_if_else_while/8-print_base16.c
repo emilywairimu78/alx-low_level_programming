b95ec8d5d6454816189c51e90f3fec1cdf9e1994
emilywairimu78 <emilywairimu78@gmail.com>
2022-10-16 13:41:26 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,17 @@
 nclude <stdio.h>
 /**
- *  * main-program entry point.
- *   * Return: 0 if no error, non zero if error.
- *    */
+ * main-program entry point.
+ * Return: 0 if no error, non zero if error.
+ */
 int main(void)
 {
-		int i;
-			char hexvalues[] = "0123456789abcdef";
+	int i;
+	char hexvalues[] = "0123456789abcdef";
 
-				for (i = 0; i < 16; i++)
-						{
-									putchar(hexvalues[i]);
-										}
-					putchar('\n');
-						return (0);
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hexvalues[i]);
+	}
+	putchar('\n');
+	return (0);
 }
